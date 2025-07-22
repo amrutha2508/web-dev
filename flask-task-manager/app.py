dd9f8e146ec212ad1f0b385ab237f0223e05eb5a
amrutha2508 <147953598+amrutha2508@users.noreply.github.com>
2025-07-22 15:28:53 -0400

    Create app.py

---
+++ flask-task-manager/app.py
@@ -0,0 +1,68 @@
+# What Happens When You Run This?
+# Flask creates a local server at http://127.0.0.1:5000/
+# Visiting that URL runs the index() function.
+# The browser displays "Hello World".
+
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test.db" # this is telling our app where the database is located everything is going to be stored in test.db in this case
+# '///' is relative path - we dont need to specify exact location , '////' is absolute path
+
+db = SQLAlchemy(app) # intialize the database with the setting from the app.
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    #completed = db.Column(db.Integer,default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self): #returns a string everytime we access the a Todo Object in the terminal in this case it returns Task id
+        return '<Task %r>' % self.id
+
+
+@app.route('/',methods=['POST','GET']) # tells Flask what URL should trigger the function., and the methods that the route can accept, 'GET' is present by default
+                #'/' is the root URL (i.e., http://localhost:5000/)
+def index(): # a view function that returns the response (in this case, the string "Hello World")
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue adding your task."
+    else: 
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html',tasks=tasks) # it knows to look into the "templates" folder, it has that name for that reason
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "There was a problem deleting that task"
+    
+@app.route('/update/<int:id>',methods=['GET','POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == "POST":
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating your task'
+    else:
+        return render_template('update.html',task = task)
+    
+
+
+if __name__ == "__main__": # Checks if this file is being run directly
+    app.run(debug=True)
