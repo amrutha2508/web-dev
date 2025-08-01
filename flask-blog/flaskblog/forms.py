159f4451e4d0a651b6b3a8d09771a05df785f94d
amrutha2508 <147953598+amrutha2508@users.noreply.github.com>
2025-07-31 21:20:13 -0400

    added profile picture upload feature

--- flask-blog/flaskblog/forms.py
+++ flask-blog/flaskblog/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError # do pip3 install email_validator, if you face any errors
 from flaskblog.models import User
@@ -26,3 +28,22 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
+#form to update account information
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2,max=20)])
+    email = StringField('Email',validators=[DataRequired(), Email()])
+    picture = FileField('Update profile picture',validators=[FileAllowed(['png','jpg'])])
+    submit = SubmitField('Update')
+    
+    # always mention correct fieldname for the functions as WTForms can identify only them 
+    def validate_username(self, username): # validate username coming from the form
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+        
+    def validate_email(self, email): # validate email 
+        if email.data != current_user.email:
+            email = User.query.filter_by(email=email.data).first()
+            if email:
+                raise ValidationError('That email is taken. Please choose a different one.')
